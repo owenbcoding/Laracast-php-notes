0bed301c83317f5a57b720cc13ff9acb4a1ca5d2
owenbcoding <eoghankb@gmail.com>
2026-06-22 19:50:42 +0100

    Update page title from 'PHP Exercise' to 'Hello PHP'

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Exercise</title>
+    <title>Hello PHP</title>
 </head>
 <body>
     <h1>
