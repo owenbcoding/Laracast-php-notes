dd263d5013469ca0bee663436e4421761eb347e0
owenbcoding <eoghankb@gmail.com>
2026-07-10 00:39:36 +0100

    Keep only practice.php ignored and restore index page

--- index.php
+++ index.php
@@ -6,16 +6,20 @@
 </head>
 <body>
     <h1>Recommended Books</h1>
-    <?php 
-    $business = [
-        "The Lean Startup" => "Eric Ries",
-        "Zero to One" => "Peter Thiel",
-        "Good to Great" => "Jim Collins"
-    ];
-
-    foreach  ($business['category'] as $book => $author) {
-        echo "<p>$book by $author</p>";
-    }
-    ?>
+        <?php 
+        $books = [
+            'Do Androids Dream of Electric Sheep?',
+            'The langoliers',
+            'Hail Mary',
+        ];
+        ?>
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+        <!-- <li>Do Androids Dream of Electric Sheep?</li>
+        <li>The langoliers</li>
+        <li>Hail Mary</li> -->
+    </ul>
 </body>
 </html>
